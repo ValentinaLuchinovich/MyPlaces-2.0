928a9d51517e2663f6e76d8f13a5025143143754
ValentinaLuchinovich <valinkontakt11@bk.ru>
2025-04-11 19:19:48 +0700

    fix: remove unused code

--- MyPlaces/Screens/Profile/Coordinator/ProfileCoordinator.swift
+++ MyPlaces/Screens/Profile/Coordinator/ProfileCoordinator.swift
@@ -21,26 +21,6 @@ final class ProfileCoordinator: Coordinator {
 extension ProfileCoordinator: TabBarChildCoordinated {
     
     public func makeTab() -> UIViewController {
-#warning("Переделать на фабрику")
-//        BackgroundViewController(VisitsFactory().build(coordinator: self), navigationController: navigationController)
         return ProfileViewController()
     }
-}
-
-extension ProfileCoordinator {
-    
-//    func showClientDetailsScreen(_ viewModel: VisitsViewModel) {
-//        let vc = VisitsClientDetailViewController(viewModel)
-//        navigationController.presentFpc(vc, animated: true, scrollView: vc.tableView, haveGrabber: true , layout: .fullScreen(20))
-//    }
-//    
-//    func showModal(delegate: PushDelegate?) {
-//        let viewController = PushTurnOnModalViewController(delegate: delegate)
-//        navigationController.presentFpc(viewController, animated: true, layout: .autoSized())
-//    }
-//    
-//    #warning("Используется для переходов по deeplink, если будет не нужно - удалить")
-//    func showAnalytic() {
-//        (navigationController.tabBarController as? TabBarController)?.tabBarButtonAction(selectedIndex: 1)
-//    }
 }
